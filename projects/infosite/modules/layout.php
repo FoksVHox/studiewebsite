2ed1188714b45c8cca97e563b03546283320859d
Foks <37844753+FoksVHox@users.noreply.github.com>
2021-01-26 09:59:54 +0100

    Fix missing routes

--- projects/infosite/modules/layout.php
+++ projects/infosite/modules/layout.php
@@ -30,7 +30,7 @@ class Layout
                         <h1 class="text-white text-3xl font-medium text-center font-sans">SoMeInfo</h1>
                     </div>
                     <div class="flex flex-col space-y-3">
-                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="/index.php">
+                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="http://elev.htxroskilde.dk/2020/jimmih20/projects/infosite/index.php">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                             </svg>
@@ -42,13 +42,13 @@ class Layout
                             </svg>
                             <span>Sociale Medier</span>
                         </a>-->
-                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="/articles.php">
+                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="http://elev.htxroskilde.dk/2020/jimmih20/projects/infosite/articles.php">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                             </svg>
                             <span>Artikler</span>
                         </a>
-                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="/help.php">
+                        <a class="block text-white font-medium py-2 px-2 bg-gray-600 rounded-lg flex justify-content items-center space-x-2" href="http://elev.htxroskilde.dk/2020/jimmih20/projects/infosite/help.php">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
                             </svg>
